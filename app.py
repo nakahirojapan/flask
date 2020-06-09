88356c05f9e6a0c599402e79d18fad0241009f02
nakahirojapan <66053634+nakahirojapan@users.noreply.github.com>
2020-06-09 21:23:38 +0900

    ヘッダーフッター

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask , render_template
 app = Flask(__name__)
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
+    
 if __name__ == "__main__":
     app.run(debug=True)
-    
-    @app.route("/test")
-    def test():
-        return render_template("test.html")
