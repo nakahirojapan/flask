7ffc822e8e4631262bfaf608c3763dd31ad3774d
nakahirojapan <66053634+nakahirojapan@users.noreply.github.com>
2020-06-09 20:29:03 +0900

    ルーティングに/testを追加

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ from flask import Flask , render_template
 app = Flask(__name__)
 if __name__ == "__main__":
     app.run(debug=True)
+    
+    @app.route("/test")
+    def test():
+        return render_template("test.html")
