af4e55d16e8b9b3a29092da909d600610d631eb4
nakahirojapan <66053634+nakahirojapan@users.noreply.github.com>
2020-06-10 19:57:08 +0900

    ２日目データベース接続

--- app.py
+++ app.py
@@ -3,7 +3,66 @@ app = Flask(__name__)
 
 @app.route("/test")
 def test():
-    return render_template("test.html")
+    name = "flask"
+    return render_template("test.html",name = name)
+    
+
+@app.route("/greet/<text>")
+def greet(text):
+    return text + "さん、こんにちは"
+
+    
+    
+@app.errorhandler(404)
+def notfound(code):
+    return "404ペーーージ"
+
+# ２日目
+@app.route("/dbtest")
+def dbtest():
+    # データベースに接続
+    conn =sqlite3.connect('flask.db')
+    # どこのデータを抜くかカーソルをあてる。カーソル→目印
+    c =conn.cursor()
+    
+    # 実行する
+    c.execute("select name,age,adress from users where id =1")
+    
+    # fetchone フェッチ　実際に取得する
+    user_info =c.fetchone()
+    # データベース接続終了
+    c.close()
+    
+    # user_info の中身を確認
+    print(user_info)
+    return render_template("dbtest.html",user_info =user_info)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)   
+ 
+    
