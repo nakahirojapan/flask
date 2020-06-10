caa5e3f58fa617fe7f38286eef9228db78f3328c
nakahirojapan <66053634+nakahirojapan@users.noreply.github.com>
2020-06-10 21:35:52 +0900

    いろいろついか

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from flask import Flask , render_template
 app = Flask(__name__)
 
@@ -26,19 +28,38 @@ def dbtest():
     c =conn.cursor()
     
     # 実行する
-    c.execute("select name,age,adress from users where id =1")
     
-    # fetchone フェッチ　実際に取得する
+    c.execute("select name,adress from users where id =3")
+    
+    # fetchone フェッチ　実際に取得する ふぇっちわん
     user_info =c.fetchone()
     # データベース接続終了
     c.close()
     
     # user_info の中身を確認
-    print(user_info)
+    # print(user_info)
     return render_template("dbtest.html",user_info =user_info)
 
+# データベースを追加
+@app.route("/add")
+def add():
+    return render_template("add.html")
 
-
+# データを追加するボタンの処理
+@app.route("/add,methods=["POST"])
+def add_post():
+        #    add.htmlからformのname="task"を取得
+    task = request.form.get("task")
+        # データベースに接続
+    conn =sqlite3.connect('flask.db') 
+    c =conn.cursor()
+    # (task,)のカンマは忘れずに！タプル型なので
+    # ?(task,)が入るよ
+    # insert into はデータを追加
+    c.execute("insert into task values(null,?)",(task,))
+    conn.commit()
+    c.close()
+    return "データを追加できました！"
 
 
 
